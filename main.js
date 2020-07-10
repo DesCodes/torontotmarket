fe4dd9f898f0df398b9bb33aa244184252c217d8
Desmond <desmond.yan.lai@gmail.com>
2020-07-10 14:27:59 -0400

    added contact box

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+	// side nav function
 	$('.bar').on('click', function(){
 		$('nav ul li').toggleClass('showMenu')
+	});
+
+	// contact us function
+	$('.closeBox, .contactUsLink').on('click', function(){
+		console.log('l')
+		$('.contactUs').toggleClass('showContactUs')
 	});
 });
