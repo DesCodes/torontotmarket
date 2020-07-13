1606d0830e8551a3d593413ff446bc34a193af96
Desmond <desmond.yan.lai@gmail.com>
2020-07-13 13:59:07 -0400

    added translation funtion

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	// $('.chinese').hide();
+	
+	// translation button
+	$('.translation .chinese').on('click', function(){
+		$('.chinese').show();
+		$('.english').hide();
+		$('.translation .english').show();
+		$('.translation .chinese').hide();
+	});
+
+	$('.translation .english').on('click', function(){
+		$('.english').show();
+		$('.chinese').hide();
+		$('.translation .chinese').show();
+		$('.translation .english').hide();
+	})
+
+
 	// side nav function
 	$('.bar').on('click', function(){
 		$('nav ul li').toggleClass('showMenu')
